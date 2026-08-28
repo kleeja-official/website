dc95ff3cae7924c640a8bbd7d26883061fced223
Mitan Omar <git@mitan.dev>
2026-08-28 06:06:40 +0200

    chore: add logo

---
+++ app.config.ts
@@ -0,0 +1,20 @@
+export default defineAppConfig({
+    docus: {
+        colorMode: 'light'
+    },
+    ui: {
+        colors: {
+            primary: 'red',
+            neutral: 'zinc'
+        }
+    },
+    header: {
+        title: 'Kleeja',
+        logo: {
+            light: '/images/logo.svg',
+            dark: '/images/logo.svg',
+            alt: 'Kleeja Logo',
+        },
+    },
+});
+
